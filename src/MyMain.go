483b4f483480776eca6b593a973ecbd10d206ccf
ThinkCats <624432528@qq.com>
2015-07-09 13:04:26 +0800

    update lib

--- src/MyMain.go
+++ src/MyMain.go
@@ -4,12 +4,21 @@ package main
 import (
 	"fmt"
 	process "lib"
+	"github.com/robfig/cron"
 )
 
-func main() {
-	fmt.Println("Hello World!")
+func doSend() {
 	str := process.Get7DaysWeather()
 	fmt.Println(str)
 	email := process.NewEmail("798207330@qq.com","新消息来啦",str)
 	process.SendEmail(email)
+}
+
+func main(){
+	fmt.Println("Begin Send")
+	c := cron.New()
+	spec := "0 2 13 * * ?"
+	c.AddFunc(spec,doSend)
+	c.Start()
+	select{}
 }
